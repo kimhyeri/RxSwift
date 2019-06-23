a59cf6e8acf50ddd5ab08babaa14542930e9da7c
hyeri <hyer1k@naver.com>
2019-06-23 20:13:34 +0900

    fetch phasset

---
+++ Lecture/CameraFilter/CameraFilter/PhotosCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosCollectionViewCell.swift
+//  CameraFilter
+//
+//  Created by hyeri kim on 23/06/2019.
+//  Copyright © 2019 hyeri kim. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+}
--- Lecture/CameraFilter/CameraFilter/PhotosCollectionViewController.swift
+++ Lecture/CameraFilter/CameraFilter/PhotosCollectionViewController.swift
@@ -12,6 +12,8 @@ import Photos
 
 class PhotosCollectionViewController: UICollectionViewController {
 
+    private var images = [PHAsset]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         populatePhotos()
@@ -19,11 +21,49 @@ class PhotosCollectionViewController: UICollectionViewController {
     }
 
     func populatePhotos() {
-        PHPhotoLibrary.requestAuthorization({ status in
+        PHPhotoLibrary.requestAuthorization({ [weak self] status in
             if status == .authorized {
+                guard let self = self else { return }
+
                 // access the photo from library
+                // only image fetching
+                
+                let assets = PHAsset.fetchAssets(with: PHAssetMediaType.image, options: nil)
+                assets.enumerateObjects { (object, count, stop) in
+                    self.images.append(object)
+                }
+                
+                // most recent image on the top
+                self.images.reverse()
+                
+                DispatchQueue.main.async {
+                    self.collectionView.reloadData()                    
+                }
             }
         })
     }
     
+}
+
+extension PhotosCollectionViewController {
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.images.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as? PhotosCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        let asset = self.images[indexPath.row]
+        let manager = PHImageManager.default()
+        manager.requestImage(for: asset, targetSize: CGSize(width: 100, height: 100), contentMode: .aspectFit, options: nil) { image, _ in
+            DispatchQueue.main.async {
+                cell.photoImageView.image = image
+            }
+        }
+        
+        return cell
+    }
 }
