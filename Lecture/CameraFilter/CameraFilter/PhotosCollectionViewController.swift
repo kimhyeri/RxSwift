723ebbe2d388c3b9b46a7b6a0be91fa63837ceb7
hyeri <hyer1k@naver.com>
2019-07-09 13:10:51 +0900

    Controller chagne

--- Lecture/CameraFilter/CameraFilter/PhotosCollectionViewController.swift
+++ Lecture/CameraFilter/CameraFilter/PhotosCollectionViewController.swift
@@ -14,20 +14,19 @@ class PhotosCollectionViewController: UICollectionViewController {
     
     // Expose observable
     private let selectedPhotoSubject = PublishSubject<UIImage>()
-    
+    private var images = [PHAsset]()
+
     var selectedPhoto: Observable<UIImage> {
         return selectedPhotoSubject.asObservable()
     }
-    
-    private var images = [PHAsset]()
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         populatePhotos()
         
     }
     
-    func populatePhotos() {
+    private func populatePhotos() {
         PHPhotoLibrary.requestAuthorization({ [weak self] status in
             if status == .authorized {
                 guard let self = self else { return }
