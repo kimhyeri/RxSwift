95f6a2cb4176bc271ef1099667c48b8e4af8feeb
hyeri <hyer1k@naver.com>
2019-07-11 03:48:39 +0900

    Setting up UI

--- Lecture/NewsMVVM/NewsMVVM/AppDelegate.swift
+++ Lecture/NewsMVVM/NewsMVVM/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        UINavigationBar.appearance().barTintColor = UIColor.gray
+        
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+
+        UINavigationBar.appearance().largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
         return true
     }
 
--- Lecture/NewsMVVM/NewsMVVM/ViewController.swift
+++ Lecture/NewsMVVM/NewsMVVM/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+       
+        setupTableView()
+        setupNavigation()
     }
 
-
+    func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func setupNavigation() {
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+        
+    }
+    
+    
+}
