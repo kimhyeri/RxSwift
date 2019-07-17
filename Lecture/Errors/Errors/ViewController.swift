24a8831aa319ee1fb9d684d329af6e85266b8b8a
hyeri <hyer1k@naver.com>
2019-07-17 23:49:51 +0900

    Create Error handling project

---
+++ Lecture/Errors/Errors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Errors
+//
+//  Created by hyeri kim on 17/07/2019.
+//  Copyright © 2019 hyeri kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Lecture/Errors/Pods/Target Support Files/Pods-Errors/Pods-Errors-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Errors : NSObject
+@end
+@implementation PodsDummy_Pods_Errors
+@end
