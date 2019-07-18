6d5c87bb26f6fb429296b01020936c9494547cdb
hyeri <hyer1k@naver.com>
2019-07-18 16:55:54 +0900

    Create weather models

---
+++ Lecture/Errors/Errors/Model/Weather.swift
@@ -0,0 +1,14 @@
+//
+//  Weather.swift
+//  Errors
+//
+//  Created by hyeri kim on 18/07/2019.
+//  Copyright © 2019 hyeri kim. All rights reserved.
+//
+
+import Foundation
+
+struct Weather: Decodable {
+    let temp: Double
+    let humidity: Double
+}
---
+++ Lecture/Errors/Errors/Model/WeatherResult.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherResult.swift
+//  Errors
+//
+//  Created by hyeri kim on 18/07/2019.
+//  Copyright © 2019 hyeri kim. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherResult: Decodable {
+    let main: Weather
+}
